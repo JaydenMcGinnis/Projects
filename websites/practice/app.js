c769dcb84d9fa782b5a607b30a2aa85c81b640cf
Jayden <jaydenmcginnis65@gmail.com>
2024-09-12 16:22:42 +1000

    Created a book object that creates instances of books

--- websites/practice/app.js
+++ websites/practice/app.js
@@ -6,9 +6,14 @@ function book(title, author, pages, read) {
     this.read = read ? "have read" : "not read yet";
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
-    }
+    };
 }
 
-let theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, false);
+book.prototype.rating = 5;
+book.prototype.showRating = function () {
+    return `Rating: ${this.rating}/10`;
+}
+
+let theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, true);
 
 console.log(theHobbit.info())
