318be8a47561e09731407932b22747363a2ebb0d
Jayden <jaydenmcginnis65@gmail.com>
2024-09-13 14:36:52 +1000

    Displays a card containing book objects keys and values in table format. Creates new card for every book object inside library list

---
+++ websites/library/app.js
@@ -0,0 +1,78 @@
+// Array of books
+const library = [];
+
+function Book(title, author, pages, read) {
+    // Book constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+let Harry_Potter_and_the_Deathly_Hallows = new Book("Harry Potter and the Deathly Hallows",
+                                                    "J. K. Rowling",
+                                                    607,
+                                                    false
+);
+
+function addToLibrary(book, library) {
+    // Add book to library
+    library.push(book);
+};
+
+addToLibrary(Harry_Potter_and_the_Deathly_Hallows, library);
+
+function displayBooks(library) {
+    // Loop through library array and create new cards
+    library.forEach(book => {
+        createCard(book);
+    });
+};
+
+// Get the main-container element
+const mainContainer = document.querySelector(".main-container");
+
+function createCard(book) {
+    // Create card div
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    // Create book title heading
+    let bookTitle = document.createElement("h3");
+    bookTitle.innerText = book.title;
+
+    // Create table for book information
+    let bookTable = document.createElement("table");
+
+    // Create table row for headers
+    let headerRow = document.createElement("tr");
+    let valueRow = document.createElement("tr");
+
+    // Object.keys(book) returns an array of property names
+    Object.keys(book).forEach(key => {
+        // Create table header for each key
+        let bookTableHead = document.createElement("th");
+        bookTableHead.innerText = key;
+
+        // Create table data for each corresponding value
+        let bookTableData = document.createElement("td");
+        bookTableData.innerText = book[key];
+
+        // Append the header and data to their respective rows
+        headerRow.appendChild(bookTableHead);
+        valueRow.appendChild(bookTableData);
+    });
+
+    // Append the rows to the table
+    bookTable.appendChild(headerRow);
+    bookTable.appendChild(valueRow);
+
+    // Assemble card by appending the title and the table
+    card.appendChild(bookTitle);
+    card.appendChild(bookTable);
+
+    // Append the card to the main container
+    mainContainer.appendChild(card);
+}
+
+displayBooks(library);
